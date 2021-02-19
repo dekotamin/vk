e510111a39da4ba6a42f58ba4a7a1cf9118f2a5c
Alina <nyrete@gmail.com>
2021-02-20 01:58:33 +0500

    Добавление методов "search","delete" в PostManager

--- src/main/java/manager/PostManager.java
+++ src/main/java/manager/PostManager.java
@@ -1,4 +1,14 @@
 package manager;
 
+import domain.Post;
+
 public class PostManager {
+    private Post[] posts;
+
+    public class Post[] search(String query, String domain, int count, int offset, int userId, int usersOnly) {
+
+        return null;
+}
+    public void delete(int userId, int postId) {
+    }
 }
